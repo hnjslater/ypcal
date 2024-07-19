36c5abcabfd115abc6e6d0f056139dccebf11eb7
henry slater <henry.slater@gmail.com>
2024-07-19 02:02:25 +0100

    Refactor

--- ypcal.cpp
+++ ypcal.cpp
@@ -8,44 +8,45 @@ const std::string RED = "\x1b[31m";
 const std::string YELLOW = "\x1b[33m";
 
 auto main(int argc, char** argv) -> int {
-    std::chrono::year current_y;
-    std::chrono::sys_days current;
     std::chrono::year_month_day today = std::chrono::time_point_cast<std::chrono::days>(std::chrono::system_clock::now());
 
+    std::chrono::year current_y;
     if (argc == 1) {
-        current = today.year()/1/1;
         current_y = today.year();
     }
     else {
         current_y = std::chrono::year{std::stoi(argv[1])};
-        current = std::chrono::sys_days{current_y/1/1};
     }
 
-    std::map<int,int> padding;
-    unsigned num_columns = 0;
-    for (unsigned i = 1; i <= 12; i++) {
-	int p = ((std::chrono::weekday{current_y/std::chrono::month{i}/1}-std::chrono::Monday).count());
-	padding[i] = p;
-	auto last_day_month = current_y/std::chrono::month{i}/std::chrono::last;
+    std::vector<int> padding;
+    int num_columns = 0;
+    for (auto current_ym = current_y/1; current_ym.year() == current_y; current_ym += std::chrono::months{1}) {
+	int p = (std::chrono::weekday{current_ym/1}-std::chrono::Monday).count();
+	padding.push_back(p);
 
-	num_columns = std::max(num_columns, p+static_cast<unsigned>(last_day_month.day()));
+	auto last_day_month = current_ym/std::chrono::last;
+	auto month_length = static_cast<unsigned>(last_day_month.day());
+	num_columns = std::max(num_columns, static_cast<int>(month_length)+p);
     }
 
     std::cout << "   ";
-    for (unsigned j = 1; j <= num_columns; j++) {
+    for (int j = 1; j <= num_columns; j++) {
 	// First two letters of each weekday name
         std::cout << std::format(" {:%a}", std::chrono::weekday(j%7)).substr(0,3);
     }
     std::cout << "\n";
 
+    std::chrono::sys_days current = current_y/1/1;
     std::chrono::year_month_day current_ymd = current_y/1/1;
+    auto current_padding = padding.begin();
     while (current_ymd.year() == current_y) {
         std::cout << NORMAL;
         std::cout << current_ymd.month() << " ";
 
-        for (int p = 0; p < padding[static_cast<unsigned>(current_ymd.month())]; p++) {
+        for (int p = 0; p < *current_padding; p++) {
             std::cout << "   ";
         }
+	current_padding++;
 
         std::chrono::month current_m = current_ymd.month();
         while (current_ymd.month() == current_m) {
@@ -67,4 +68,5 @@ auto main(int argc, char** argv) -> int {
 
         std::cout << "\n";
     }
+    std::cout << NORMAL;
 }
